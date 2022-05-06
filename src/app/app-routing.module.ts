664ccbeb7d1a07554c364cb5b88386e69fb7047d
Eduarda Matias <evcm2003@gmail.com>
2022-05-06 10:03:26 -0300

    alterando menu, e criando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'inicio', pathMatch: 'full'},
+  {path: 'menu', component: MenuComponent},
   {path: 'inicio', component: InicioComponent},
   {path:'entrar', component: LoginComponent },
   {path: 'cadastrar', component: CadastrarComponent},
