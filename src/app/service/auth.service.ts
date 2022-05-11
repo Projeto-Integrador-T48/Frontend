3f2bea49a9b927208b119b268506dae47a291650
Matheus Ferreira <matheus@java.com>
2022-05-11 11:32:33 -0300

    Service postagem

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -9,7 +9,18 @@ import { UsuarioLogin } from '../model/UsuarioLogin';
   providedIn: 'root',
 })
 export class AuthService {
+  
   constructor(private http: HttpClient) {}
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  refreshToken(){
+    this.token = {
+      headers: new HttpHeaders().set('Authorization', environment.token)
+    }
+  }
 
   entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin> {
     return this.http.post<UsuarioLogin>(
@@ -23,6 +34,10 @@ export class AuthService {
       'https://conectagen.herokuapp.com/usuario/cadastrar',
       usuario
     );
+  }
+
+  getByIdUser(id: number): Observable<Usuario>{
+    return this.http.get<Usuario>(`https://conectagen.herokuapp.com/usuario/${id}`, this.token)
   }
 
   logado() {
