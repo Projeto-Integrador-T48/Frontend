0884ca46678c83cf28f2e10cb39e91c35f2f9cda
Fernando de Oliveira Carvalho <fernandooliveiracarvalho98@gmail.com>
2022-05-11 13:36:56 -0300

    get e post do tema

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 
 @Component({
@@ -8,10 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemaComponent implements OnInit {
 
+  tema: Tema = new Tema();
+  listaTemas: Tema[];
+
   constructor(
+    private router: Router,
+    private temaService: TemaService
   ) { }
 
   ngOnInit() {
+    if(environment.token == ''){
+      this.router.navigate(['/entrar'])
+    }
+    this.findAllTemas()
   }
 
+   findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[])=>{
+        return this.listaTemas = resp
+    })
+  }
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+      this.tema = resp
+      alert('Tema cadastrado com sucesso!')
+      this.findAllTemas()
+      this.tema = new Tema()
+    });
+  }
 }
