b3ee7e90da876c5b5105832bc3523968740a8b39
Janaina <janainag.teixeira@outlook.com>
2022-05-13 20:33:57 -0300

    Fix: Cadastrar component

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -16,40 +16,29 @@ export class CadastrarComponent implements OnInit {
   confirmarSenha: string;
   tipodeUsuario: string;
 
-  usuario: Usuario = new Usuario
-  confirmarSenha:string;
-  tipodeUsuario: string
-  
   constructor(
     private auth: AuthService,
     private router: Router,
     private alertas: AlertasService
-    ) { 
-  
-=======
-  constructor(private auth: AuthService, private router: Router) {}
+  ) {}
 
   ngOnInit() {
     window.scroll(0, 0);
-
   }
 
   confirmSenha(event: any) {
     this.confirmarSenha = event.target.value;
   }
 
-  tipoUsuario(event: any) {
-    this.tipodeUsuario = event.target.value;
-  }
   cadastrar() {
     this.usuario.tipo = this.tipodeUsuario;
     if (this.usuario.senha != this.confirmarSenha) {
-      alert('As senhas estão incorretas');
+      this.alertas.showAlertDanger('As senhas estão incorretas');
     } else {
       this.auth.cadastrar(this.usuario).subscribe((resp: Usuario) => {
         this.usuario = resp;
         this.router.navigate(['/entrar']);
-        alert('Usuário cadastrado com sucesso');
+        this.alertas.showAlertSuccess('Usuário cadastrado com sucesso');
       });
     }
   }
@@ -57,30 +46,19 @@ export class CadastrarComponent implements OnInit {
     let txtNome = <HTMLLabelElement>document.querySelector('#txtNome');
     let nome = <HTMLInputElement>document.querySelector('#nome');
 
-
-tipoUsuario(event: any){
-this.tipodeUsuario = event.target.value
-}
-cadastrar(){
-  this.usuario.tipo= this.tipodeUsuario
-  if(this.usuario.senha != this.confirmarSenha){
-    this.alertas.showAlertDanger("As senhas estão incorretas")
-  }else{
-    this.auth.cadastrar(this.usuario).subscribe((resp: Usuario) =>{
-      this.usuario = resp;
-      this.router.navigate(["/entrar"])
-      this.alertas.showAlertSuccess("Usuário cadastrado com sucesso")
-    })
-
-    if (this.usuario.nome.length < 2) {
-      txtNome.innerHTML = 'Digite um nome válido';
-      txtNome.style.color = '#dc3545';
-      nome.style.border = 'solid 1px #dc3545';
-    } else {
-      txtNome.innerHTML = 'Nome válido';
+    if (nome.value.length > 0) {
       txtNome.style.color = '#198754';
-      nome.style.border = 'solid 1px #198754';
+      nome.style.borderColor = '#198754';
+      txtNome.innerHTML = 'Senha válida';
+    } else {
+      txtNome.style.color = '#dc3545';
+      nome.style.borderColor = '#dc3545 ';
+      txtNome.innerHTML = 'Senha inválida';
     }
+  }
+
+  tipoUsuario(event: any) {
+    this.tipodeUsuario = event.target.value;
   }
 
   validaEmail() {
@@ -89,59 +67,42 @@ cadastrar(){
     let email = <HTMLInputElement>document.querySelector('#usuario');
     if (this.usuario.usuario.match(regex)) {
       txtEmail.innerHTML = 'Email válido';
-      txtEmail.style.color = '#198754'
+      txtEmail.style.color = '#198754';
       email.style.border = 'solid 1px #198754';
     } else {
       txtEmail.innerHTML = 'Email inválido';
-      txtEmail.style.color = '#dc3545'
+      txtEmail.style.color = '#dc3545';
       email.style.border = 'solid 1px #dc3545';
     }
   }
 
+  validaSenha() {
+    let senhaLabel = <HTMLLabelElement>document.querySelector('#senhaLabel');
+    let senhaInput = <HTMLInputElement>document.querySelector('#senha');
 
-validaEmail() {
-  let regex = '[a-z0-9]+@[a-z]+.[a-z]{2,3}';
-  let txtEmail = <HTMLLabelElement>document.querySelector('#txtEmail');
-  let email = <HTMLInputElement>document.querySelector('#usuario');
-  if (this.usuario.usuario.match(regex)) {
-    txtEmail.innerHTML = 'Usuário';
-    email.style.border = 'solid 1px green';
-  } else {
-    txtEmail.innerHTML = 'Usuário Invalido';
-    email.style.border = 'solid 1px red';
-
-  validaSenha(){
-    let senhaLabel = (<HTMLLabelElement>document.querySelector('#senhaLabel'))
-    let senhaInput = (<HTMLInputElement>document.querySelector('#senha'))
-
-    if(senhaInput.value.length > 0){
-      senhaLabel.style.color = '#198754'
-      senhaInput.style.borderColor = '#198754'
+    if (senhaInput.value.length > 0) {
+      senhaLabel.style.color = '#198754';
+      senhaInput.style.borderColor = '#198754';
       senhaLabel.innerHTML = 'Senha válida';
-      
-    }else{
-      senhaLabel.style.color = '#dc3545'
-      senhaInput.style.borderColor = '#dc3545 '
+    } else {
+      senhaLabel.style.color = '#dc3545';
+      senhaInput.style.borderColor = '#dc3545 ';
       senhaLabel.innerHTML = 'Senha inválida';
     }
-
   }
-  
-  validaConfirmaSenha(){
-    let senhaLabel = (<HTMLLabelElement>document.querySelector('#senhaLabel2'))
-    let senhaInput = (<HTMLInputElement>document.querySelector('#confirmaSenha'))
 
-    if(senhaInput.value.length > 0){
-      senhaLabel.style.color = '#198754'
-      senhaInput.style.borderColor = '#198754'
+  validaConfirmaSenha() {
+    let senhaLabel = <HTMLLabelElement>document.querySelector('#senhaLabel2');
+    let senhaInput = <HTMLInputElement>document.querySelector('#confirmaSenha');
+
+    if (senhaInput.value.length > 0) {
+      senhaLabel.style.color = '#198754';
+      senhaInput.style.borderColor = '#198754';
       senhaLabel.innerHTML = 'Senha válida';
-      
-    }else{
-      senhaLabel.style.color = '#dc3545'
-      senhaInput.style.borderColor = '#dc3545 '
+    } else {
+      senhaLabel.style.color = '#dc3545';
+      senhaInput.style.borderColor = '#dc3545 ';
       senhaLabel.innerHTML = 'Senha inválida';
     }
-
-
   }
 }
